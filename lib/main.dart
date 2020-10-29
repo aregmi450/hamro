637bccb5f00ce1febcd649de7057e8c89e28a47f
Arun Regmi <aregmi450@gmail.com>
2020-10-29 20:49:01 +0545

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red[400],
-          title: Text('My First App'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text(
-            'Hello People !',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-            ),
+      home: Home(),
+    ));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red[400],
+        title: Text('My First App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          'Hello People !',
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Text('Click'),
-          backgroundColor: Colors.red[600],
-        ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
