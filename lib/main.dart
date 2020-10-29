3f3a9d01cca281de3eaf371ce32799bb79a1063b
Arun Regmi <aregmi450@gmail.com>
2020-10-29 20:09:28 +0545

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return StreamProvider<User>.value(
-      value: AuthService().user,
-      child: MaterialApp(
-        home: HomePage(),
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red[400],
+          title: Text('My First App'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text(
+            'Hello People !',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('Click'),
+          backgroundColor: Colors.red[600],
+        ),
       ),
-    );
-  }
-}
+    ));
